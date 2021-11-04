4ee4aad721a9012d87ad0f13eb187f11d179850b
Gautami Avinash Shetty <92733988+ShettyGA@users.noreply.github.com>
2021-11-05 00:53:37 +0530

    Add files via upload

---
+++ 09_C_Program_to_print_multipication_table_in_a_given_range.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int row = 0, column = 0, s_num = 0, e_num = 0;
+    printf("Enter the range's starting number: \n");
+    scanf("%d",&s_num);
+    printf("Enter the range's ending number: \n");
+    scanf("%d",&e_num);
+
+    for(row = 1; row <= 10; row++)
+    {
+        for(column = s_num; column<= e_num; column++)
+        {
+            printf("%2d ",row*column);
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
